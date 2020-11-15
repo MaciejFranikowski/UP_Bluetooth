2bae10579851ebf15653040288617d3e8466c7bf
Maciej Franikowski <mfranikowski@gmail.com>
2020-11-15 16:31:23 +0100

    Added the options to choose BT adapter, scan for devices and simple pairing

---
+++ BluetoothUP/BluetoothLogic.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace BluetoothUP
+{
+    class BluetoothLogic : INotifyPropertyChanged
+    {
+        // Bluetooth adapters on the server
+        public ObservableCollection<BluetoothRadio> BtAdapters { get; set; }
+        // Chosen bluetooth adapter on the server
+        public BluetoothRadio CurrentAdapter
+        {
+            get { return currentAdapter; }
+            set
+            {
+                currentAdapter = value;
+                this.OnPropertyChanged("CurrentAdapter");
+            }
+        }
+        private BluetoothRadio currentAdapter;
+
+        // Creating the servers I think
+        private BluetoothEndPoint localEndPoint;
+        // The client
+        private BluetoothClient client;
+        // Available bt devices
+        public ObservableCollection<BluetoothDeviceInfo> BluetoothDevices { get; set; }
+        // Chosen BT device
+
+        public BluetoothDeviceInfo ChosenDevice 
+        {
+            get { return chosenDevice; }
+            set
+            {
+                chosenDevice = value;
+                this.OnPropertyChanged("ChosenDevice");
+            }
+        }
+        private BluetoothDeviceInfo chosenDevice;
+
+
+        public BluetoothLogic() 
+        {
+            BtAdapters = new ObservableCollection<BluetoothRadio>();
+            BluetoothDevices = new ObservableCollection<BluetoothDeviceInfo>();
+        }
+        public void UpdateAdapters()
+        {
+            BtAdapters.Clear();
+            BluetoothRadio[] temp = BluetoothRadio.AllRadios;
+           
+            foreach (BluetoothRadio adapter in temp)
+            {
+                BtAdapters.Add(adapter);
+            }
+
+            if (BtAdapters.Any())
+            {
+                CurrentAdapter = BtAdapters[0];
+            }
+            else
+            {
+                MessageBox.Show("No adapters were found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+                
+
+        }
+
+        public void SearchForDevices()
+        {
+            BluetoothDevices.Clear();
+            localEndPoint = new BluetoothEndPoint(CurrentAdapter.LocalAddress, BluetoothService.SerialPort);
+            client = new BluetoothClient(localEndPoint);
+            BluetoothDeviceInfo[] temp = client.DiscoverDevices();
+            foreach (BluetoothDeviceInfo device in temp)
+            {
+                BluetoothDevices.Add(device);
+            }
+           
+            if (BluetoothDevices.Any())
+            {
+                ChosenDevice = BluetoothDevices[0];
+            }
+            else
+            {
+                MessageBox.Show("No bluetooth devices were found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public void ConnectToDevice()
+        {
+            try
+            {
+                BluetoothSecurity.PairRequest(ChosenDevice.DeviceAddress, "123456");
+            }
+            catch
+            {
+                Console.WriteLine("Couldn't pair with" + ChosenDevice.DeviceName.ToString());
+            }
+        }
+
+        #region INotifyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+        #endregion
+    }
+
+}
---
+++ BluetoothUP/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace BluetoothUP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BluetoothLogic btL;
+        public MainWindow()
+        {
+            // order is very important!!
+            InitializeComponent();
+            btL = new BluetoothLogic();
+            this.DataContext = btL;
+        }
+
+        private void ClickAdapters_Button(object sender, RoutedEventArgs e)
+        {
+            btL.UpdateAdapters();
+        }
+
+        private void ClickSearch_Button(object sender, RoutedEventArgs e)
+        {
+            btL.SearchForDevices();
+        }
+
+        private void ClickPair_Button(object sender, RoutedEventArgs e)
+        {
+            btL.ConnectToDevice();
+        }
+    }
+
+    
+}
